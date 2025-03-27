55cde271dba11d72c02f4446a6720314e1263f6c
quanphatdev <lequanphat3579@gmail.com>
2025-03-27 11:53:07 +0700

    feat(quanphat): setup strapi starter kit

---
+++ src/api/product/controllers/product.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * product controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::product.product");
+
